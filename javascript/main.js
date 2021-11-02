c42bb8d10986341478c15776ce38470c1be46664
andrewras93 <andrewras93@gmail.com>
2021-11-02 13:15:12 +0100

    Printed questions to page.

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,25 @@
 'use strict'
 import {createHeader} from './modules/navigationheader.js';
-import {doSomethingInQuiz} from '././showquizz.js';
+import {displayQuiz} from '././showquizz.js';
+import {Quiz} from "./modules/quiz.js";
+import { QuizzQuestions } from "./modules/quizquestions.js";
 export const $ = function (bar) { return document.getElementById(bar); };
+
+const question1 = new QuizzQuestions('Sport', 'Hvem vandt EM i fodbold i 1992?', 'Danmark');
+question1.addAnswer(['Danmark', 'Frankrig', 'Tyskland', 'Polen']);
+
+const question2 = new QuizzQuestions('Sport', 'Dette er en test?', 'Test');
+question2.addAnswer(['Test 1', 'Test 2', 'Test 3', 'Test 4']);
+
+const quizOne = new Quiz ('Sport', new Date);
+
+quizOne.addQuestion(question1);
+quizOne.addQuestion(question2);
+
+const quiz1 = document.getElementById('quiz1');
+
 const init = function () {
     createHeader();
-    doSomethingInQuiz();
+    displayQuiz(quizOne, quiz1);
 }
 window.addEventListener("load", init);
--- javascript/showquizz.js
+++ javascript/showquizz.js
@@ -1,11 +1,25 @@
 'use strict'
-import {Quiz} from "./modules/quiz.js";
-import {$} from './main.js';
-//this one connects and handle all classes and puts it on the html.
-// const displayQuiz = function (quiz, where)
 
+export function displayQuiz(quiz, where) {
+    const quizTitle = document.getElementById('quizTitle');
 
+    quizTitle.innerHTML = quiz.name;
 
-export function doSomethingInQuiz(){
+    for (let i = 0; i < quiz.questions.length; i++) {
+        const h3 = document.createElement('h3');
+        const h2 = document.createElement('h2');
 
+        h2.appendChild(document.createTextNode(`${i + 1} / ${quiz.questions.length}`));
+        h3.appendChild(document.createTextNode(`Spørgsmål ${i + 1}: ${quiz.questions[i].question}`));  
+
+        where.append(h2, h3);
+
+        quiz.questions[i].possibleAnswers[0].forEach(element => {
+            const button = document.createElement('button');
+
+            button.appendChild(document.createTextNode(element));
+
+            where.appendChild(button);
+        });
+    }
 }
