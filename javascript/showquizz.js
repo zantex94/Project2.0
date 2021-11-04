ae4aa8ad267d4a2257b473412f1eed4f60edc846
andrewras93 <andrewras93@gmail.com>
2021-11-04 13:05:41 +0100

    Pushed.

--- javascript/showquizz.js
+++ javascript/showquizz.js
@@ -1,15 +1,13 @@
 'use strict'
+import { Quiz } from './modules/createQuiz.js';
 import { quizOne } from './modules/quiz1Questions.js';
 import { quizTwo } from './modules/quiz2Questions.js';
-// for handle localstorage.
-// localStorage.setItem("player",JSON.stringify(person[0]));
-
-//this one connects and handle all classes and puts it on the html.
-// const displayQuiz = function (quiz, where)
 
 const checkedButtons = document.getElementsByClassName('checked');
 const submitButton = document.getElementById('submit');
-const results = document.getElementById('results');
+const answerButton = document.getElementsByClassName('answerButton');
+
+console.log(answerButton);
 
 let isTrue = false;
 
@@ -21,28 +19,90 @@ export function displayQuiz(quiz, where) {
     const submitButton = document.getElementById('submit');    
 
     for(let i = 0; i < answerButton.length; i++) {
-        answerButton[i].addEventListener('click', function() {
+        /*answerButton[i].addEventListener('click', function() {
             answerButton[i].classList.toggle('checked');    
             
             validate(quiz);
-        });
+        });*/      
+        //console.log('Hallo')
     }
+
+    submitButton.addEventListener('click', function(e) {
+        
+        if(isTrue) {
+            let string = JSON.stringify(quiz);
+            localStorage.setItem("quiz", string);
+            
+
+        } else {
+            e.preventDefault();
+            alert('Du mangler at svare på et eller flere spørgsmål.');
+        }
+    })
 
     
 }
 
 function validate(quiz) {
-    console.log(`${checkedButtons.length} / ${quiz.questions.length}`);  
 
-    if (checkedButtons.length === quiz.questions.length) {
+    for(let i = 0; i < answerButton.length; i++) {
+        if (answerButton[i].checked) {
+            console.log('Hallo')
+        }
+    }
+
+    
+    //console.log(`${answerButton.checked} / ${quiz.questions.length}`);  
+
+    /*if (answerButton.checked === quiz.questions.length) {
         isTrue = true;      
     } else {
         isTrue = false;
-    }
+    }*/
+}
+
+function displayResults(where) {
+
+    let quiz = localStorage.getItem('quiz');
+    let object = JSON.parse(quiz);    
+
+    let display = toStringResults(object);    
+
+    where.innerHTML = display;
+
+}
+
+function toStringResults(o) {
+    let s = "";
+        
+        for(let i = 0; i < o.questions.length; i++){
+            s += "<h2>";
+            s += "Spørgsmål " + (i + 1) + " ud af " + o.questions.length;
+            s += "</h2>";
+            s += `<div class='questionDiv'`;
+            s += "<p class='questionP'>";
+            s += o.questions[i].question;
+            s += "</p>";
+            s += "</div>";
+            s += `<div class="answerDiv" id=group${i}>`;            
+        
+            for(let j = 0; j < o.questions[i].possibleAnswers.length; j++) {
+                //s += `<input type='radio' name='group${i}' class="answerButton" value='${this.questions[i].possibleAnswers[j]}'>`;
+                s += '<label>';
+                s += o.questions[i].possibleAnswers[j];
+                s += '</label>';
+            }
+            s += '</div>';
+
+        }
+
+        return s;
+
 }
 
 export function doSomething(){
 
+    const results = document.getElementById('results');
     const index = document.getElementById('index');
     const quiz1 = document.getElementById('quiz1');
     const quiz2 = document.getElementById('quiz2');
@@ -54,6 +114,9 @@ export function doSomething(){
     }
     if (quiz2) {
         displayQuiz(quizTwo, quiz2);
+    }
+    if (results) {
+        displayResults(results);
     }
 
 
